8e78f3527e9527f6629d4ed83beb9aa78f130580
AR <angelinrajan20@gmail.com>
2023-05-07 15:48:48 -0400

    Javascript.js file has been added

---
+++ prework-study-guide/assets/script.js
@@ -0,0 +1,35 @@
+
+ /*
+if (topic === 'HTML') {
+ console.log("Let's study HTML!");
+} else if (topic === 'CSS') {
+ console.log("Let's study CSS!");
+} else if (topic === 'Git') {
+ console.log("Let's study Git!");
+} else if (topic === 'JavaScript') {
+ console.log("Let's study JavaScript!");
+} else {
+ console.log('Please try again!');
+}
+*/
+ /*var shapes = ["triangle", "square", "pentagon", "circle"];
+ for(var x = 0; x<shapes.length; x++) {
+    console.log(shapes[x]);
+}
+ //this is one way of getting all values in console: console.log(shapes);
+This is another way of adding all values to console:
+ var shapes = ["triangle", "square", "pentagon", "circle"];
+console.log(shapes[0]);
+console.log(shapes[1]);
+console.log(shapes[2]);
+console.log(shapes[3]);
+
+ //For loop is the most efficient way to log all the shapes in console
+ //x++ is a shortcut way of writing x+1. this allows the array to increase by 1
+ */
+
+ var topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
+ for(var a = 0; a<topics.length; a++) {
+    console.log(topics[a]);
+ }
+ 
